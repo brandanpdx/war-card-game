8eb80ec8fe137120944975ca741b825228ff95bd
Brandan Sayarath <brandan@brandan.tech>
2020-04-28 21:11:51 -0700

    Initial Commit

---
+++ WarCardGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by Brandan Sayarath on 4/28/20.
+//  Copyright © 2020 Brandan Sayarath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
